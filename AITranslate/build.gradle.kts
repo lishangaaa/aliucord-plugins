67598ceca75a3a8de5cb466d2e44fb1907ad03ee
lishangaaa <lishangabc@gmail.com>
2026-08-30 23:05:24 +0800

    更新 build.gradle.kts

--- AITranslate/build.gradle.kts
+++ AITranslate/build.gradle.kts
@@ -1,4 +1,4 @@
-version = "1.0.5"
+version = "1.0.4"
 description = "Translate Discord messages using any OpenAI/DeepSeek compatible LLM API."
 
 aliucord {
@@ -7,7 +7,7 @@ aliucord {
     author("Tyman", 487443883127472129L)
 
     changelog.set("""
-        # Version 1.0.5
-        * 无
+        # Version 1.0.4
+        * 优化精简插件
     """.trimIndent())
 }
