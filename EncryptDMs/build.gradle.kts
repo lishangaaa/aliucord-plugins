2e1c6441f03139c23688c0ef225982a6d743cd4c
lishangaaa <lishangabc@gmail.com>
2026-08-30 14:10:08 +0800

    Delete EncryptDMs directory

--- EncryptDMs/build.gradle.kts
+++
@@ -1,7 +0,0 @@
-version = "0.0.1"
-description = "Encryption go brrrrrrrrrrrrr"
-aliucord.changelog.set("""
-    # Version 1.0.0
-    * Initial release
-""".trimIndent())
-aliucord.excludeFromUpdaterJson.set(true)
